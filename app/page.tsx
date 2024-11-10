ce2f0c1750e929663c998674b3d146f4f5c82409
Killian <killian.boularand@outlook.fr>
2024-11-10 17:23:14 +0100

    first commit

---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div className="w-full grid place-content-center min-h-screen">
+      <h1 className="text-red-500 text-3xl">Yunus Ecommerce</h1>
+    </div>
+  );
+}
